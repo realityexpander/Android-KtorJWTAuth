1634c480f57e4e9d353a6e052d72cbeae81a3c42
Chris Athanas <realityexpander@gmail.com>
2022-06-13 15:23:54 -0500

    Add scroll to focus experiments

--- app/src/main/java/com/realityexpander/androidktorjwtauth/ui/AuthScreen.kt
+++ app/src/main/java/com/realityexpander/androidktorjwtauth/ui/AuthScreen.kt
@@ -1,30 +1,37 @@
 package com.realityexpander.androidktorjwtauth.ui
 
 import android.widget.Toast
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.navigationBarsWithImePadding
-import com.google.accompanist.insets.statusBarsHeight
-import com.google.accompanist.insets.statusBarsPadding
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.realityexpander.androidktorjwtauth.auth.AuthResult
 import com.realityexpander.androidktorjwtauth.ui.destinations.AuthScreenDestination
 import com.realityexpander.androidktorjwtauth.ui.destinations.AuthenticatedScreenDestination
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 @Destination(start = true)
 fun AuthScreen(
@@ -34,9 +41,21 @@ fun AuthScreen(
     val state = viewModel.state
     val context = LocalContext.current
 
+    val focusManager = LocalFocusManager.current
+
+
+    val bringIntoViewRequesters = mutableListOf(remember { BringIntoViewRequester() })
+    val buttonViewRequester = remember { BringIntoViewRequester() }
+    val coroutineScope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    repeat(13) {
+        bringIntoViewRequesters += remember { BringIntoViewRequester() }
+    }
+
     LaunchedEffect(viewModel, context) {
         viewModel.authResults.collect { result ->
-            when(result) {
+            when (result) {
                 is AuthResult.Success -> {
                     Toast.makeText(context, "Success. " + result.data, Toast.LENGTH_LONG).show()
                 }
@@ -48,8 +67,10 @@ fun AuthScreen(
                     }
                 }
                 is AuthResult.Unauthorized -> {
-                    Toast.makeText(context,
-                        "Unauthorized. " + (result.data?.toString() ?: ""), Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        context,
+                        "Unauthorized. " + (result.data?.toString() ?: ""), Toast.LENGTH_LONG
+                    ).show()
                 }
                 is AuthResult.Error -> {
                     Toast.makeText(context, result.errorMessage, Toast.LENGTH_LONG).show()
@@ -58,25 +79,146 @@ fun AuthScreen(
         }
     }
 
-    ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
 
-        Column(
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .verticalScroll(rememberScrollState()),
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//
+//        repeat(6) { PlaceholderCard() }
+//
+//        Card(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(16.dp)
+//                .bringIntoViewRequester(bringIntoViewRequester),
+//            elevation = 8.dp
+//        ) {
+//            Column {
+//                Text(modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp), text = "Please fill something in")
+//                TextField(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(16.dp)
+//                        .onFocusEvent { focusState ->
+//                            if (focusState.isFocused) {
+//                                coroutineScope.launch {
+//                                    bringIntoViewRequester.bringIntoView()
+//                                }
+//                            }
+//                        },
+//                    placeholder = { Text(text = "Some input") },
+//                    value = state.signInPassword,
+//                    singleLine = true,
+//                    onValueChange = { viewModel.onEvent(AuthUiEvent.SignInPasswordChanged(it)) },
+//                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+//                    keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() })
+//                )
+//                Button(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(start = 16.dp, bottom = 16.dp, end = 16.dp),
+//                    onClick = { /*Nothing*/ }) {
+//                    Text("Click")
+//                }
+//            }
+//        }
+//
+//        repeat(10) { PlaceholderCard() }
+//    }
+
+    fun requestBringIntoView(focusState: FocusState, viewItem: Int) {
+        if (focusState.isFocused) {
+            coroutineScope.launch {
+                delay(200)
+                if (viewItem >= 2) {
+                    buttonViewRequester.bringIntoView()
+                } else {
+                    bringIntoViewRequesters[viewItem].bringIntoView()
+                }
+            }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .imePadding()
+            .padding(10.dp)
+            .verticalScroll(rememberScrollState())
+
+    ) {
+
+        repeat(12) { i ->
+            Row(
+                modifier = Modifier
+                    .bringIntoViewRequester(bringIntoViewRequesters[i]),
+            ) {
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+                    modifier = Modifier
+                        .onFocusEvent { focusState ->
+                            requestBringIntoView(focusState, i)
+                        },
+                )
+            }
+        }
+
+
+        Button(
+            onClick = {},
             modifier = Modifier
-                .padding(32.dp)
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background)
-                .statusBarsPadding()
-                .navigationBarsWithImePadding()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .bringIntoViewRequester(buttonViewRequester)
+        ) {
+            Text(text = "Yeah Visible")
+        }
+    }
+
+
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//    ) {
+//        LazyColumn( // Workaround to ensure softkeyboard will scroll up the view
+//            Modifier.weight(.5f),
+//            verticalArrangement = Arrangement.Top,
+//        ) {}
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .imePadding()
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .bringIntoViewRequester(bringIntoViewRequesters[0]),
         ) {
             TextField(
                 value = state.signUpUsername,
                 onValueChange = {
                     viewModel.onEvent(AuthUiEvent.SignUpUsernameChanged(it))
                 },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusEvent { focusState ->
+                        requestBringIntoView(focusState, 0)
+                    },
                 placeholder = {
                     Text(
                         text = "Username",
@@ -84,70 +226,109 @@ fun AuthScreen(
                     )
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .bringIntoViewRequester(bringIntoViewRequesters[1]),
+        ) {
             TextField(
                 value = state.signUpPassword,
                 onValueChange = {
                     viewModel.onEvent(AuthUiEvent.SignUpPasswordChanged(it))
                 },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusEvent { focusState ->
+                        requestBringIntoView(focusState, 1)
+                    },
                 placeholder = {
                     Text(text = "Password")
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    viewModel.onEvent(AuthUiEvent.SignUp)
-                },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "Sign up")
-            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                viewModel.onEvent(AuthUiEvent.SignUp)
+            },
+        ) {
+            Text(text = "Sign up")
+        }
+        Spacer(modifier = Modifier.height(32.dp))
 
-            Spacer(modifier = Modifier.height(64.dp))
 
+        Row(
+            modifier = Modifier
+                .bringIntoViewRequester(bringIntoViewRequesters[2]),
+        ) {
             TextField(
                 value = state.signInUsername,
                 onValueChange = {
                     viewModel.onEvent(AuthUiEvent.SignInUsernameChanged(it))
                 },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusEvent { focusState ->
+                        requestBringIntoView(focusState, 2)
+                    },
                 placeholder = {
                     Text(text = "Username")
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .bringIntoViewRequester(bringIntoViewRequesters[3]),
+        ) {
             TextField(
                 value = state.signInPassword,
                 onValueChange = {
                     viewModel.onEvent(AuthUiEvent.SignInPasswordChanged(it))
                 },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusEvent { focusState ->
+                        requestBringIntoView(focusState, 3)
+                    },
                 placeholder = {
                     Text(text = "Password")
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    viewModel.onEvent(AuthUiEvent.SignIn)
-                },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "Sign in")
-            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                viewModel.onEvent(AuthUiEvent.SignIn)
+            },
+            modifier = Modifier
+                .bringIntoViewRequester(buttonViewRequester),
+        ) {
+            Text(text = "Sign in")
+
         }
     }
 
     if (state.isLoading) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             CircularProgressIndicator()
         }
     }
 }
+
+//@Composable
+//private fun PlaceholderCard() {
+//    Card(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(16.dp),
+//        elevation = 8.dp
+//    ) {
+//        Text(modifier = Modifier.padding(32.dp), text = "Placeholder")
+//    }
+//}
--- app/src/main/java/com/realityexpander/androidktorjwtauth/ui/MainActivity.kt
+++ app/src/main/java/com/realityexpander/androidktorjwtauth/ui/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.realityexpander.androidktorjwtauth.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.realityexpander.androidktorjwtauth.R
 import com.realityexpander.androidktorjwtauth.ui.theme.AndroidKtorJWTAuthTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
         setContent {
             AndroidKtorJWTAuthTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+                Surface(
+                    color = Color.Black,
+                    modifier = Modifier.fillMaxSize()
+                ) { //MaterialTheme.colors.background) {
+
+//                    Scaffold {
+                        DestinationsNavHost(navGraph = NavGraphs.root)
+//                    }
                 }
             }
         }
